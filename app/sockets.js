1f6bfaa6d8dbf24ecc660f80ad6e34e895802ae4
pringon <gojedan98@yahoo.com>
2017-05-14 13:01:04 +0300

    Fix bug with users list not updating on user connection

--- app/sockets.js
+++ app/sockets.js
@@ -39,7 +39,7 @@ module.exports = (io) => {
 
 let emitUsersList = (socket, message, users) => {
 
-  socket.broadcast.emit('user connection', { message: message,
+  io.sockets.emit('user connection', { message: message,
                                              users: users })
 }
 
